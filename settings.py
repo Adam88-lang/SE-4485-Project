ae930f438159027e95b66db58491125b6fe3cb3a
Adam88-lang <83882835+Adam88-lang@users.noreply.github.com>
2023-04-16 19:33:50 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+SEARCH_KEY = "AIzaSyDX_GeYwWPGXnPEXRdJ-NGWAOyQC1f9o_k"
+SEARCH_ID = "a700ce1f32d4d4e9e"
+COUNTRY = "us"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 20
+
