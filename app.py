284c6bdfc2ae7ae5247734955a5f7788eb524c93
zsweet01 <116773676+zsweet01@users.noreply.github.com>
2023-04-29 19:48:59 -0500

    Restyled Front End

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 styles = """
 <style>
     body {
-
+        background: #F5F5F5;
+        font-family:  sans-serif;
     }
     .site, .snippet, .rel-button {
     .site, .snippet {
@@ -42,7 +43,112 @@ styles = """
         font-size: .8rem;
         color: green;
     }
+    .ellipse1 {
+        /* Ellipse 1 - orange*/
+        position: absolute;
+        top: calc(350px - 497px);
+        left: calc(350px - 497px);
+        width: 447px;
+        height: 418px;
+        background: #D17837;
+        border-radius: 50%;
+    }
+    .ellipse2 {
+        /* Ellipse 2 */
+        position: absolute;
+        top: 5%;
+        left: 70%;
+        width: 385px;
+        height: 340px;
+        background: #FAFAFA;
+        border-radius: 50%;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+    .ellipse3 {
+        /* Ellipse 3 */
+        position: absolute;
+        top: 60%;
+        left: 80%;
+        width: 396.95px;
+        height: 382.15px;
+        background: #FAFAFA;
+        border-radius: 50%;
+        box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+        transform: rotate(0.02deg);
+    }
 
+    .large-rectangle {
+        position: absolute;
+        display: flex;
+        flex-direction: row;
+        top: 10%;
+        left: 10%;
+        width: 80%;
+        background: #FAFAFA;
+        z-index: 0;
+    }
+    .large-rectangle-initial {
+        position: absolute;
+        top: 10%;
+        left: 10%;
+        width: 80%;
+        height: 600px;
+        background: #FAFAFA;
+        z-index: 0;
+  
+    }
+    .search-bar-initial {
+        flex: 50%;
+        padding-top: 10%;
+        text-align: center;
+        z-index: 1;
+        top: 0;
+    }
+
+   .search-bar-area {
+        flex: 50%;
+        padding-top: 20px;
+        text-align: center;
+        z-index: 1;
+        top: 0;
+        display: flex;
+        flex-direction: column;
+    }
+    
+    
+    .search-bar img {
+        position: relative;
+        text-align: center;
+    } 
+    
+    .results-below-search-bar {
+        /* Results below Search Bar */
+        position: relative;
+        left: 10%;
+        width: 80%;
+        text-align: left;
+    }
+    
+    .results-area { 
+        flex: 50%;
+        padding-top: 20px;
+        text-align: center;
+        z-index: 1;
+        top: 0;
+        display: flex;
+        flex-direction: column;
+        row-gap: 10px;
+    }
+    
+    .related-rectangles {
+        /* Related Rectangle */
+        font-size: 14px;
+        position: relative;
+        left: 10%;
+        width: 80%;
+        background: #F5F5F5;
+        text-align: left;
+    }
     .snippet {
         font-size: .9rem;
         color: gray;
@@ -60,34 +166,7 @@ styles = """
         transition: transform 0.3s ease-in-out;
         cursor: pointer;
     }
-    .split {
-        height: 100%;
-        width: 50%;
-        position: fixed;
-        z-index: 1;
-        top: 0;
-        overflow-x: hidden;
-        padding-top: 20px;
-    }
-
-    /* Control the left side */
-    .left {
-        left: 0;
-        background-color: white;
-    }
-
-    /* Control the right side */
-    .right {
-        right: 0;
-        background-color: lightgrey;
-    }
-    .centered {
-        position: absolute;
-        top: 40%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        text-align: center;
-    }
+    
     .rounded-search {
         border-radius: 25px;
         border: 4px solid lightgrey;
@@ -96,6 +175,8 @@ styles = """
         width: 400px;
         height: 44px;
     }
+    
+
 
     input[type="text"] {
         padding: 10px;
@@ -104,6 +185,9 @@ styles = """
     }
 </style>
 <script>
+
+
+
 function submitSearch(query) {
     const searchForm = document.getElementById('search-form');
     searchForm.elements.namedItem('query').value = query;
@@ -130,27 +214,73 @@ search_template = styles + """
   <head>
   </head>
   <body>
-    <div class="split left">
-        <div class="centered">
-            <img src = "https://i.ibb.co/B2fV1m2/logo.png"
-            alt = "SearchFCG" height = "200" width = "200" />
+        <div class="ellipse1">   </div>
+        <div class="ellipse2">   </div>
+        <div class="ellipse3">   </div>
+        <div class="large-rectangle-initial"> 
+                <div class="search-bar-initial">
+                <img src = "https://i.ibb.co/B2fV1m2/logo.png" alt = "SearchFCG" height = "200" width = "200" />
 
-            <form id="search-form" action="/" method="post">
-                <input class="rounded-search" type="text" name="query" placeholder="Enter Search Here">
-                <input type="submit" value="Search">
-            </form>
+                <form id="search-form" action="/" method="post">
+                    <input class="rounded-search" type="text" name="query" placeholder="Enter Search Here">
+                    <input type="submit" value="Search">
+                </form>
+                </div>
+           
+            <div class="results-area"> 
+            </div>
         </div>
-    </div>
+      
+  
   </body>
 </html>
-<div class="split right">
 """
-result_template = """
+
+
+result_search_template1 = styles + """
+<html>
+  <head>
+  </head>
+  <body>
+        <div class="ellipse1">   </div>
+        <div class="ellipse2">   </div>
+        <div class="ellipse3">   </div>
+        """
+
+result_search_template2 = """
+        <div class="large-rectangle"> 
+            <div class="search-bar-area"> 
+                <div class="search-bar">
+                    <img src = "https://i.ibb.co/B2fV1m2/logo.png" alt = "SearchFCG" height = "200" 
+                    width = "200" />
+                    <form id="search-form" action="/" method="post">
+                        <input class="rounded-search" type="text" name="query" placeholder="Enter Search Here">
+                        <input type="submit" value="Search">
+                    </form>
+                </div>
+                <div class="results-below-search-bar">
+
+"""
+
+end_search_template = """
+                </div>
+            </div>
+        <div class="results-area"> 
+
+"""
+right_side_template = """
+
+"""
+
+relevant_result_template = """
 <p class="site">{rank}: {link} <span class="rel-button" onclick='relevant("{query}", "{link}");'>Relevant</span></p>
 <a href="{link}">{title}</a>
 <p class="snippet">{snippet}</p>
-<div class="related">
-    <h2>Related Keywords:</h2>
+"""
+
+related_result_template = """
+<div class="related-rectangles">
+    <h3>Related Keywords:</h3>
     <ul>
     {related}
     </ul>
@@ -158,8 +288,10 @@ result_template = """
 """
 
 end_right_template = """
-    <p> </p>
-</div>
+       </div>
+  </body>
+</html>
+
 """
 
 
@@ -179,7 +311,9 @@ def run_search(query):
     fi = Filter(results)
     filtered = fi.filter()
     print(f"Filtered results: {filtered}")  # Add this line to debug filtered results
-    rendered = search_template
+    leftrendered = ""
+    rightrendered = ""
+    more_elipses_html = ""
     filtered["snippet"] = filtered["snippet"].apply(lambda x: html.escape(x))
     for index, row in filtered.iterrows():
         related = get_related_keywords(row["title"])
@@ -187,8 +321,18 @@ def run_search(query):
         for keyword in related:
             related_html += f'<li><a href="javascript:void(0)" onclick="submitSearch(\'{keyword}\')">{keyword}</a></li>'
         row["related"] = related_html
-        rendered += result_template.format(**row)
-    return rendered + end_right_template
+        leftrendered += relevant_result_template.format(**row)
+        rightrendered += related_result_template.format(**row)
+        if index > 0 and index % 5 == 0:
+            elipsetop1 = 500 * index/3
+            elipsetop2 = elipsetop1 + 100
+            elipsetop3 = elipsetop1 + 500
+            more_elipses_html += f'<div class="ellipse1" style="top:{elipsetop1}px"></div>'
+            more_elipses_html += f'<div class="ellipse2" style="top:{elipsetop2}px"></div>'
+            more_elipses_html += f'<div class="ellipse3" style="top:{elipsetop3}px"></div>'
+
+    full_template = result_search_template1 + more_elipses_html + result_search_template2 + leftrendered + end_search_template  + right_side_template + rightrendered + end_right_template
+    return full_template
 
 
 # Main route to handle the search form and display results
